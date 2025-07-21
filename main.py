bb4e9bf7cc11911ef4622b50a9619b408da95390
Govind Somadas <87228559+govindas3045@users.noreply.github.com>
2025-07-21 21:13:15 +0100

    Add feature to download list of tickers and filters ones with todays release datemain.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import requests
+import csv
+from datetime import date
 import smtplib
 from email.message import EmailMessage
 
+'''Downloading the CSV and getting tickers whose reportDate = Todays Date'''
+today = str(date.today())
+tickers_to_send = []
+
+CSV_URL = f'https://www.alphavantage.co/query?function=EARNINGS_CALENDAR&horizon=3month&apikey=demo'
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        if row[2] == today:
+            tickers_to_send.append(row[0])
+            print(row)
+
+'''Sending the email'''
 # Email configuration
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
@@ -12,10 +31,10 @@ recipients = ['govindplaystation4028@gmail.com', 'psdj1997@gmail.com']
 
 # Create the email
 msg = EmailMessage()
-msg['Subject'] = 'Test Email'
+msg['Subject'] = 'Tickers to Check'
 msg['From'] = sender_email
-msg['To'] = ', '.join(recipients)  # Display in "To" header
-msg.set_content('Open it. https://www.youtube.com/watch?v=sVMW45oGP7s')
+msg['To'] = ', '.join(recipients)
+msg.set_content(f'{tickers_to_send}')
 
 try:
     # Connect to SMTP server and send email
